a9bcf4b54805d436c4f06f684425a7897e97bb76
wbinsci-sudo <wbinsci@gmail.com>
2026-06-17 16:24:01 -0600

    Actualizar dependencias de Composer y agregar soporte para GeoIP y CA Bundle

--- backend/vendor/composer/autoload_psr4.php
+++ backend/vendor/composer/autoload_psr4.php
@@ -6,5 +6,10 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'MaxMind\\WebService\\' => array($vendorDir . '/maxmind/web-service-common/src/WebService'),
+    'MaxMind\\Exception\\' => array($vendorDir . '/maxmind/web-service-common/src/Exception'),
+    'MaxMind\\Db\\' => array($vendorDir . '/maxmind-db/reader/src/MaxMind/Db'),
+    'GeoIp2\\' => array($vendorDir . '/geoip2/geoip2/src'),
+    'Composer\\CaBundle\\' => array($vendorDir . '/composer/ca-bundle/src'),
     'App\\' => array($baseDir . '/src'),
 );
