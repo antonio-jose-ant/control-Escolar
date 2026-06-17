360ed4660b4ab5216deb6f0d30634b1c424c56c7
wbinsci-sudo <wbinsci@gmail.com>
2026-06-16 19:47:40 -0600

    Add Composer autoloading and initial configuration files

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd1e6e9376f5bf678e87e7e3355e61e907fe44b3f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd1e6e9376f5bf678e87e7e3355e61e907fe44b3f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
